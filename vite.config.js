828e26d8cf3b87be7a2e828e86f5ee712411b47e
sairamsamala <sairamsamala63@gmail.com>
2026-06-26 14:34:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smart-pos-Using-React/',
+  base: './', // <-- Change this to a dot and a slash
 })
